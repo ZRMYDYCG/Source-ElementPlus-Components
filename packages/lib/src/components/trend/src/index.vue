b1165f566ed9e7a385f8d2abf3719dfea6a5cb43
ZRMYDYCG <547471919@qq.com>
2025-01-14 20:17:19 +0800

    feat: 支持插槽定制文字及传值定制，同时配置图标色的颜色反转功能

--- packages/lib/src/components/trend/src/index.vue
+++ packages/lib/src/components/trend/src/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { withDefaults, defineProps, useSlots } from "vue"
+
 const props = withDefaults(
   defineProps<{
     /*趋势类型*/
@@ -9,19 +11,28 @@ const props = withDefaults(
     upColor: string
     /*下降趋势的图标颜色*/
     downColor: string
+    /*颜色反转 只在默认颜色下生效 如果使用了自定义颜色 该属性无效*/
+    reverseColor: boolean
   }>(),
   {
     type: "up",
     text: "趋势",
-    upColor: "#57c5f7",
-    downColor: "#f04134",
+    // 默认情况下，根据 reverseColor 决定颜色
+    upColor: () => (props.reverseColor ? "#f04134" : "#57c5f7"),
+    downColor: () => (props.reverseColor ? "#57c5f7" : "#f04134"),
+    reverseColor: true,
   },
 )
+
+const slots = useSlots()
 </script>
 
 <template>
   <div class="items-center gap-1 inline-flex mr-2">
-    <div>{{ text }}</div>
+    <div>
+      <slot v-if="slots.default"></slot>
+      <div v-else>{{ text }}</div>
+    </div>
     <div class="icon">
       <el-icon-arrowup
         :style="{ color: upColor }"
